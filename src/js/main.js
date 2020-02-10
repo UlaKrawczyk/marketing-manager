d194e6abd81388e8bba3561c01e0b6253cf984af
Ula Krawczyk <ula.krawczyk7@gmail.com>
2020-02-10 20:25:40 +0100

    feat: hamburger menu - js logic and styling part 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,66 @@
 "use strict";
+
+const body = document.body;
+const banner = document.querySelector(".banner");
+const nav = document.querySelector(".nav");
+const navHamburger = document.querySelector(".nav__hamburger");
+const navMenu = document.querySelector(".nav__menu");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let bannerWidth;
+let lastScroll = 0;
+
+//szerokość nawigacji
+function setNavWidth() {
+  bannerWidth = banner.offsetWidth;
+  nav.style.width = bannerWidth + "px";
+}
+window.onload = function () {
+  setNavWidth();
+};
+window.onresize = function () {
+  setNavWidth();
+}
+
+//navigation revealing or hiding depanding on scroll direction
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    //down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    //up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
+
+//nawigacja - hamburger menu
+navHamburger.addEventListener('click', function () {
+  const isOpened = navHamburger.getAttribute('aria-expanded') === 'true';
+
+  navHamburger.classList.toggle('btn-menu_open', !isOpened);
+  navHamburger.setAttribute('aria-expanded', String(!isOpened));
+  navMenu.classList.toggle('nav__menu_open', !isOpened);
+});
+
+//zmiana wyglądu hamburgera po lekkim zescrollowaniu w dół
+function changeHamburger() {
+  const mq835 = window.matchMedia('(max-width: 835px)');
+  const scrolledDown = window.pageYOffset;
+
+  if (mq835.matches && scrolledDown > 40) {
+    navHamburger.classList.add("btn-menu_scrolled");
+  } else if (!mq835.matches && scrolledDown > 100) {
+    navHamburger.classList.add("btn-menu_scrolled");
+  } else {
+    navHamburger.classList.remove("btn-menu_scrolled");
+  }
+}
+window.addEventListener("scroll", changeHamburger);
